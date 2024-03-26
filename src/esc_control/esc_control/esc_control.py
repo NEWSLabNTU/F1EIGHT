e993aa5ad69824d33d82678e0331b568ceb91701
aeon <jerry73204@gmail.com>
2024-03-26 17:00:58 +0800

    add report sent to autoware

--- src/esc_control/esc_control/esc_control.py
+++ src/esc_control/esc_control/esc_control.py
@@ -3,6 +3,8 @@ import rclpy
 from rclpy.node import Node
 from autoware_auto_control_msgs.msg import AckermannControlCommand
 from Adafruit_PCA9685 import PCA9685
+from autoware_auto_vehicle_msgs.msg import SteeringReport
+from autoware_auto_vehicle_msgs.msg import VelocityReport
 
 # Initialize the PCA9685 using the default address (0x40).
 pwm = PCA9685(address=0x40, busnum=7)
@@ -18,8 +20,8 @@ stop = 380  # No throttle value accepted by the ESC
 # This section sets the default values for the steering servo
 steering_value = 360
 steering_init = 360
-steering_max_left = 260
-steering_max_right = 500
+steering_max_left = 240
+steering_max_right = 480
 reverse = 0
 
 class Esc_control(Node):
@@ -28,14 +30,32 @@ class Esc_control(Node):
         self.subscription = self.create_subscription(
             AckermannControlCommand,
             '/external/selected/control_cmd',
+            #'/control/command/control_cmd',
             self.listener_callback,
             10
         )
 
+        self.pub1 = self.create_publisher(SteeringReport, 'vehicle/status/steering_status', 10)
+        self.pub2 = self.create_publisher(VelocityReport, 'vehicle/status/velocity_status', 10)
+
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        steering_report = SteeringReport()
+        steering_report.steering_tire_angle = 0.0
+        self.pub1.publish(steering_report)
+
+        velocity_report = VelocityReport()
+        velocity_report.longitudinal_velocity = 0.0
+        velocity_report.header.stamp = self.get_clock().now().to_msg()
+        self.pub2.publish(velocity_report)
+
     def listener_callback(self, msg):
         global reverse
         # Access the speed from the longitudinal part of the message
-        speed = int(msg.longitudinal.speed) + stop
+        speed = int(msg.longitudinal.speed*2) + stop
         if speed < stop and reverse >= -40:
             speed = stop+reverse
             reverse -= 1
@@ -51,11 +71,12 @@ class Esc_control(Node):
         # Set the PCA9685 servo controller (dc motor and steering servo)
         if revmax < speed < fwdmax:
             pwm.set_pwm(0, 0, speed)
-            print("speed:", speed)
+            self.get_logger().info(f"speed: {speed} autoware value: {msg.longitudinal.speed}")
 
         if steering_max_left < steering_value < steering_max_right:
             pwm.set_pwm(1, 0, steering_value)
-            print("steering_value:", steering_value)
+            self.get_logger().info(f"steering_value: {steering_value}  autoware value: {msg.lateral.steering_tire_angle}")
+ 
 
 def main(args=None):
     rclpy.init(args=args)
