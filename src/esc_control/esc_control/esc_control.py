a173c3eed7051870e4a34d7e65f8a749e63f8823
aeon <jerry73204@gmail.com>
2024-03-28 13:43:48 +0800

    publish traffic_signal topic to autoware

--- src/esc_control/esc_control/esc_control.py
+++ src/esc_control/esc_control/esc_control.py
@@ -5,6 +5,9 @@ from autoware_auto_control_msgs.msg import AckermannControlCommand
 from Adafruit_PCA9685 import PCA9685
 from autoware_auto_vehicle_msgs.msg import SteeringReport
 from autoware_auto_vehicle_msgs.msg import VelocityReport
+from autoware_perception_msgs.msg import TrafficSignalArray
+from autoware_perception_msgs.msg import TrafficSignalElement
+from autoware_perception_msgs.msg import TrafficSignal
 
 # Initialize the PCA9685 using the default address (0x40).
 pwm = PCA9685(address=0x40, busnum=7)
@@ -27,6 +30,8 @@ reverse = 0
 class Esc_control(Node):
     def __init__(self):
         super().__init__('esc_control')
+        
+        # subscribe control command from autoware
         self.subscription = self.create_subscription(
             AckermannControlCommand,
             #'/external/selected/control_cmd',
@@ -34,10 +39,15 @@ class Esc_control(Node):
             self.listener_callback,
             10
         )
-
+	
+	
+	# publish vehicle status to autoware
         self.pub1 = self.create_publisher(SteeringReport, 'vehicle/status/steering_status', 10)
         self.pub2 = self.create_publisher(VelocityReport, 'vehicle/status/velocity_status', 10)
-
+        
+        # publish traffic signal to autoware
+        self.tl_pub = self.create_publisher(TrafficSignalArray, 'perception/traffic_light_recognition/traffic_signals', 10)
+        
         timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
@@ -51,6 +61,20 @@ class Esc_control(Node):
         velocity_report.longitudinal_velocity = 0.0
         velocity_report.header.stamp = self.get_clock().now().to_msg()
         self.pub2.publish(velocity_report)
+        
+        tse = TrafficSignalElement()
+        tse.color = 3 #GREEN
+        tse.shape = 1 #CIRCLE
+        tse.status = 2 #SOLID_ON
+        tse.confidence = 1.0
+        ts = TrafficSignal()
+        ts.traffic_signal_id = 1
+        ts.elements.append(tse)
+        tsa = TrafficSignalArray()
+        tsa.stamp = self.get_clock().now().to_msg()
+        tsa.signals.append(ts)
+        self.tl_pub.publish(tsa)
+        
 
     def listener_callback(self, msg):
         global reverse
